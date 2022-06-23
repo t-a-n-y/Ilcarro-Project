7afbb1fddb6408cb8abd256503e5a759f1bb833c
t-a-n-y <panko-tanya@yandex.ru>
2022-06-23 10:14:49 +0300

    HomeWorkIlcarro

---
+++ Ilcarro-Project/src/test/java/tests/BaseTests.java
@@ -0,0 +1,55 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTests {
+
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition(){
+        wd = new ChromeDriver();
+        wd.navigate().to("https://ilcarro-1578153671498.web.app/search");
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        wd.quit();
+    }
+
+    public void openLoginForm(){
+        wd.findElement(By.xpath("//a[@href='/login?url=%2Fsearch']")).click();
+
+    }
+
+    public void fillLoginForm(String email, String password){
+        //find+click+clear+sendKey
+        type(By.cssSelector("#email"),email);
+
+        //find+click+clear+sendKey
+        type(By.cssSelector("#password"),password);
+    }
+
+    public void submitYalla(){
+        wd.findElement(By.xpath("//button[@type='submit']"));
+    }
+
+    public void type(By locator, String text){
+        if(text!=null) {
+            WebElement element = wd.findElement(locator);
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+        }
+    }
+}
---
+++ Ilcarro-Project/src/test/java/tests/LoginTests.java
@@ -0,0 +1,40 @@
+package tests;
+import org.testng.annotations.Test;
+
+
+public class LoginTests extends BaseTests{
+
+
+    @Test
+    public void successLogin(){
+        openLoginForm();
+        fillLoginForm("tanya@gmail.ru","Ttanya123$");
+        submitYalla();
+        //Assert
+
+    }
+
+    @Test
+    public void loginNegativeTestWrongLogin(){
+        openLoginForm();
+        fillLoginForm("tanyagmail.ru","Ttanya123$");
+        submitYalla();
+        //Assert
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
